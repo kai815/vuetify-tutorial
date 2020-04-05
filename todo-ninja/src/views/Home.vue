8c24b3c9ab2f093756e80816b85252b8c4794c6f
kai815 <h.kaizuka815@gmail.com>
2020-04-05 11:57:30 +0900

    apply style button and icon

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-<div class="home">
-  <h1>HomePage</h1>
-  <p class="red white--text">This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify".</p>
-  <p class="pink lighten-4 red--text">This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify".</p>
-  <p class="pink darken-4 red--text text--lighten-3">This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify". This is the homepage for studying "Vuetify".</p>
-  <h1 class="display-4">Massive Display</h1>
-  <h4 class="display-1">Smaller Display</h4>
-  <p class="headline">This is a headline</p>
-  <p class="subheading font-weight-bold">This is a subheading</p>
-  <p class="caption text-uppercase">This is a caption</p>
-</div>
+  <div class="home">
+    <h1>HomePage</h1>
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn text color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>EMAIL ME</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>EMAIL ME</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
