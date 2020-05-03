24ed304529034d37f6de61dc33da18640a95fcdf
kai815 <h.kaizuka815@gmail.com>
2020-05-03 11:01:19 +0900

    ceate button with breakpoint

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,28 +1,9 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn text color="pink">click me</v-btn>
-
-    <v-btn depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>EMAIL ME</span>
-    </v-btn>
-
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>mdi-email</v-icon>
-      <span>EMAIL ME</span>
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">
-      <span>EMAIL ME</span>
-      <v-icon right large>mdi-email</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
+    <v-btn class="hidden-sm-only">click me</v-btn>
   </div>
 </template>
 
